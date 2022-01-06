e8cc216130b495251d5b49889ed233a54025a239
C O K L U K <siberup@yandex.com>
2022-01-07 00:14:49 +0300

    x

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+	if(!localStorage.getItem('header')) {
+     $.post('https://my.siberup.com/nav.php', JSON.stringify({}), function(data){
+	   localStorage.setItem('header', data);
+	   $("header").html(data);
+     });
+	 
+	 }else {
+		$("header").html(localStorage.getItem('header'));
+	 }
+	 if(!localStorage.getItem('footer')) {
+       $.post('https://my.siberup.com/footer.php', JSON.stringify({}), function(data){
+         localStorage.setItem('footer', data);
+		 $("footer").html(data);
+       });
+	 } else {
+	     $("footer").html(localStorage.getItem('footer'));
+	 }
